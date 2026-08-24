a25088261f8e0018d0a400aeb193c3bed8354f12
Kevin Wingfield <kwingfiel23@hotmail.com>
2026-08-24 06:05:05 -0400

    Add Docker init files and update project config

---
+++ src/main/java/com/example/Java_Docker/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.Java_Docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Java-Docker!";
+    }
+}
